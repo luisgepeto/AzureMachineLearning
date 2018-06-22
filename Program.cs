3faac9d9413c466ab4b8c28c2b8b70c64067699d
Luis Becerril <luisgepeto@gmail.com>
2018-06-22 15:33:59 -0500

    added changes for batch execution api

--- Program.cs
+++ Program.cs
@@ -5,8 +5,12 @@ namespace AzureMachineLearning
     public class Program
     {
         public static void Main(string[] args)
-        {            
-            if (args.ElementAtOrDefault(0) == "csv")
+        {        
+            if (args.ElementAtOrDefault(0) == "batch")
+            {
+                BatchExecutionApi.GetBatchPrediction();
+            }
+            else if (args.ElementAtOrDefault(0) == "csv")
             {
                 if(!int.TryParse(args.ElementAtOrDefault(1), out var numberOfRecords)) numberOfRecords = 1000;
                 RequestResponseApi.GetMultiplePrediction(numberOfRecords);
